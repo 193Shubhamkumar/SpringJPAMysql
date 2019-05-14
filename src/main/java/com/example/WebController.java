b9c7ac2036d1e9b35ab23143fcf590c585d6b192
193Shubhamkumar <luckyshubhamiimt@gmail.com>
2019-05-14 13:01:54 +0530

    Commit Spring JPA Mysql project

---
+++ src/main/java/com/example/WebController.java
@@ -0,0 +1,59 @@
+package com.example;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class WebController {
+	@Autowired
+	CustomerRepository repository;
+	
+	@RequestMapping("/save")
+	public String process(){
+		// save a single Customer
+		repository.save(new Customer("shubham", "kumar"));
+		
+		// save a list of Customers
+		repository.save(Arrays.asList(new Customer("shubham", "Chauhan"), new Customer("pradyumnna", "satapaty"),
+										new Customer("Netstorm", "Netdiagnostics"), new Customer("Shubham", "Rajvanshi")));
+		
+		return "Done";
+	}
+	
+	
+	@RequestMapping("/findall")
+	public String findAll(){
+		String result = "";
+		
+		for(Customer cust : repository.findAll()){
+			result += cust.toString() + "<br>";
+		}
+		
+		return result;
+	}
+	
+	@RequestMapping("/findbyid")
+	public String findById(@RequestParam("id") long id){
+		String result = "";
+		result = repository.findOne(id).toString();
+		return result;
+	}
+	
+	@RequestMapping("/findbylastname")
+	public String fetchDataByLastName(@RequestParam("lastname") String lastName){
+		String result = "";
+		
+		for(Customer cust: repository.findByLastName(lastName)){
+			result += cust.toString() + "<br>"; 
+		}
+		
+		return result;
+	}
+}
+
